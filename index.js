27eae657083cec1dd860ed13445ad5102dccfb4d
zanadoski <zanadoskiwork@gmail.com>
2026-04-12 21:57:14 -0500

    almost done with modal actually

---
+++ index.js
@@ -0,0 +1,26 @@
+// template_wh5m9kb
+// service_cus86fd
+// q4_ORmaCwFKvo3zvM
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+        .sendForm(
+            'service_cus86fd',
+            'template_wh5m9kb',
+            event.target
+            'q4_ORmaCwFKvo3zvM'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        })  .catch(() =>  {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at zanadoskiwork@gmail.com "
+            );
+        })
+}
