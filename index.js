ca580974c855bd1992aa822047c98eb170a4b8a0
zanadoski <zanadoskiwork@gmail.com>
2026-04-13 20:56:02 -0500

    now modal is done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function contact(event) {
         .sendForm(
             'service_cus86fd',
             'template_wh5m9kb',
-            event.target
+            event.target,
             'q4_ORmaCwFKvo3zvM'
         ).then(() => {
             loading.classList.remove("modal__overlay--visible");
@@ -23,4 +23,14 @@ function contact(event) {
                 "The email service is temporarily unavailable. Please contact me directly at zanadoskiwork@gmail.com "
             );
         })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
